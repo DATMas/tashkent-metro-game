2b3541dffafd0dd610dad54ced96acf36216c3dd
DATMas <75481988+DATMas@users.noreply.github.com>
2026-06-23 00:08:52 +0100

    stylesheet, json, gitignore and js created

---
+++ js/game.js
@@ -0,0 +1,6 @@
+﻿fetch('data/stations.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log('Lines loaded:', data.lines.length);
+    console.log('Stations loaded:', data.stations.length);
+  });
